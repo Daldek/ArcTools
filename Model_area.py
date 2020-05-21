8e5bce0377cd07c9fa662ae64d0907717e12a854
Daldek <38719982+Daldek@users.noreply.github.com>
2020-05-21 11:42:18 +0200

    Bugs fixed

--- Model_area.py
+++ Model_area.py
@@ -1,10 +1,11 @@
 import arcpy
-arcpy.CheckOutExtension("Spatial")
 from arcpy.sa import *
+
+arcpy.CheckOutExtension("Spatial")
 
 # input data
 workspace = arcpy.GetParameterAsText(0)  # Geodatabase
-dem = arcpy.GetParameterAsText(1)  # Raster
+elevation_model = arcpy.GetParameterAsText(1)  # Raster
 chosen_catchments = arcpy.GetParameterAsText(2)  # Polygons
 buildings = arcpy.GetParameterAsText(3)  # Polygons
 landuse = arcpy.GetParameterAsText(4)  # Raster
@@ -13,46 +14,56 @@ landuse = arcpy.GetParameterAsText(4)  # Raster
 output_folder = arcpy.GetParameterAsText(5)  # Not a geodatabase
 
 # Local variables
-buildings_elev = workspace + r"/buildings_elev"
-rasterized_buildings_clip = workspace + r"/rasterized_buildings_clip"
-rasterized_buildings = workspace + r"/rasterized_buildings"
-rasterized_buildings_calc = workspace + r"/rasterized_buildings_calc"
-dem_buildings = workspace + r"/dem_buildings"
-landuse_clip = workspace + r"/landuse_clip"
-roughness_grid = workspace + r"/roughness_grid"
+field_name = "elevaton"
+inclination = 30
+
+# DEM
 catchment = workspace + r"/catchment"
 catchment_buffer = workspace + r"/catchment_buffer"
 rasterized_buffer = workspace + r"/rasterized_buffer"
+buildings_elev = workspace + r"/buildings_elev"
+rasterized_buildings = workspace + r"/rasterized_buildings"
+rasterized_buildings_calc = workspace + r"/rasterized_buildings_calc"
 clipped_dem = workspace + r"/clipped_dem"
-slope_grid = workspace + r"/slope_grid"
-slope_roughness = workspace + r"/slope_roughness"
+dem_buildings = workspace + r"/dem_buildings"
 
-field_name = "elevaton"
+# Land use grid
+land_use_clip = workspace + r"/land_use_clip"
+
+# Roughness grid
+slope_grid = workspace + r"/slope_grid"
+steep_slopes_grid = workspace + r"/steep_slopes_grid"
+steep_slopes_grid_clip = workspace + r"/steep_slopes_grid_clip"
 
 # Env settings
 arcpy.env.workspace = workspace
 arcpy.env.scratchWorkspace = workspace
 arcpy.env.overwriteOutput = True
-arcpy.env.snapRaster = dem
-arcpy.env.cellSize = dem
+arcpy.env.snapRaster = elevation_model
+arcpy.env.cellSize = elevation_model
 arcpy.env.nodata = "NONE"
 
 # DEM extent
-left = arcpy.GetRasterProperties_management(dem, "LEFT")
-bottom = arcpy.GetRasterProperties_management(dem, "BOTTOM")
-right = arcpy.GetRasterProperties_management(dem, "RIGHT")
-top = arcpy.GetRasterProperties_management(dem, "TOP")
+left = arcpy.GetRasterProperties_management(elevation_model, "LEFT")
+bottom = arcpy.GetRasterProperties_management(elevation_model, "BOTTOM")
+right = arcpy.GetRasterProperties_management(elevation_model, "RIGHT")
+top = arcpy.GetRasterProperties_management(elevation_model, "TOP")
 extent = str(left) + " " + str(bottom) + " " + str(right) + " " + str(top)  # does not look nice
 
 # DEM cell size
-cell_size_x_direction = arcpy.GetRasterProperties_management(dem, "CELLSIZEX")
-cell_size_y_direction = arcpy.GetRasterProperties_management(dem, "CELLSIZEY")
+cell_size_x_direction = arcpy.GetRasterProperties_management(elevation_model, "CELLSIZEX")
+cell_size_y_direction = arcpy.GetRasterProperties_management(elevation_model, "CELLSIZEY")
 cell_size_x_direction = float(cell_size_x_direction.getOutput(0))
 cell_size_y_direction = float(cell_size_y_direction.getOutput(0))
 cell_size = (cell_size_x_direction + cell_size_y_direction) / 2
 if cell_size_x_direction != cell_size_y_direction:
     arcpy.AddMessage("Cell size in the x-direction is different from cell size in the y-direction!")
 
+
+'''
+NEW DIGITAL ELEVATION MODEL
+'''
+# Catchment
 # Add field
 arcpy.AddField_management(chosen_catchments, field_name, "SHORT")
 arcpy.AddMessage("New field has been created.")
@@ -65,6 +76,7 @@ arcpy.AddMessage("Values have been assigned.")
 arcpy.Dissolve_management(chosen_catchments, catchment, field_name, "", "MULTI_PART")
 arcpy.AddMessage("New catchment has been created.")
 
+# Buildings
 # Add field
 arcpy.AddField_management(buildings, field_name, "SHORT")
 arcpy.AddMessage("New field has been created.")
@@ -78,41 +90,19 @@ arcpy.FeatureToRaster_conversion(buildings, field_name, rasterized_buildings, ce
 arcpy.AddMessage("Buildings have been rasterized.")
 
 # Raster calculator
-calc = Raster(dem) + Raster(rasterized_buildings)
+calc = Raster(elevation_model) + Raster(rasterized_buildings)
 calc.save(rasterized_buildings_calc)
-arcpy.AddMessage("New building elevations have been calculated.")
-
-# Clip
-arcpy.Clip_management(rasterized_buildings_calc, extent, rasterized_buildings_clip, catchment, "#", "ClippingGeometry",
-                      "NO_MAINTAIN_EXTENT")
-arcpy.AddMessage("Buildings have been clipped.")
+arcpy.AddMessage("New elevations have been calculated.")
 
 # Mosaic to new raster
-arcpy.MosaicToNewRaster_management("rasterized_buildings_clip; clipped_dem", workspace, "dem_buildings", "",
+dem_buildings_input = str("rasterized_buildings_calc; ") + str(elevation_model)
+arcpy.MosaicToNewRaster_management(dem_buildings_input, workspace, "dem_buildings", "#",
                                    "32_BIT_FLOAT", cell_size, 1, "FIRST", "FIRST")
 arcpy.AddMessage("New DEM has been built.")
 
-# Slope
-arcpy.Slope_3d(dem_buildings, slope_grid, "DEGREE", 1.0)
-arcpy.AddMessage("Slopes have been calculated.")
-
-# Reclassify
-additional_roughness = Reclassify(slope_grid, "Value", RemapRange([[0, 30, "NODATA"], [30, 180, 998]]))
-additional_roughness.save(slope_roughness)
-arcpy.AddMessage("Raster has been reclassified.")
-
+# Model area
 # Clip
-arcpy.Clip_management(landuse, extent, landuse_clip, catchment, "#", "ClippingGeometry",
-                      "NO_MAINTAIN_EXTENT")
-arcpy.AddMessage("Land use raster has been clipped.")
-
-# Mosaic to new raster
-arcpy.MosaicToNewRaster_management("slope_roughness; landuse_clip", workspace, "roughness_grid", "",
-                                   "32_BIT_FLOAT", cell_size, 1, "FIRST", "FIRST")
-arcpy.AddMessage("Roughness raster has been built.")
-
-# Clip
-arcpy.Clip_management(dem, extent, clipped_dem, catchment, "999", "ClippingGeometry", "NO_MAINTAIN_EXTENT")
+arcpy.Clip_management(dem_buildings, extent, clipped_dem, catchment, "999", "ClippingGeometry", "NO_MAINTAIN_EXTENT")
 arcpy.AddMessage("DEM has been clipped.")
 
 # Buffer
@@ -125,21 +115,74 @@ arcpy.FeatureToRaster_conversion(catchment_buffer, field_name, rasterized_buffer
 arcpy.AddMessage("Buffer has been rasterized.")
 
 # Mosaic to new raster
-arcpy.MosaicToNewRaster_management("rasterized_buffer; dem_buildings", workspace, "ModelArea", "", "32_BIT_FLOAT",
-                                   cell_size, 1, "FIRST", "FIRST")
-arcpy.AddMessage("Raster has been built.")
+arcpy.MosaicToNewRaster_management("clipped_dem; rasterized_buffer", workspace, "model_domain_grid", "#",
+                                   "32_BIT_FLOAT", cell_size, 1, "FIRST", "FIRST")
+arcpy.AddMessage("Domain raster has been built.")
 
-ModelArea = workspace + r"/ModelArea"
+# model_domain_grid = workspace + r"/model_domain_grid"
 
-# # Raster to ASCII
-# arcpy.RasterToASCII_conversion(ModelArea, model_area)
-# arcpy.AddMessage("Raster has been exported to ASCII.")
+'''
+LAND USE GRID
+'''
+# Clip
+arcpy.Clip_management(landuse, extent, land_use_clip, catchment, "#", "ClippingGeometry",
+                      "NO_MAINTAIN_EXTENT")
+arcpy.AddMessage("Land use raster has been clipped.")
+
+# Mosaic to new raster
+arcpy.MosaicToNewRaster_management("land_use_clip; rasterized_buffer", workspace, "landuse_grid", "#",
+                                   "16_BIT_UNSIGNED", cell_size, 1, "FIRST", "FIRST")
+arcpy.AddMessage("Lands use raster has been built.")
+
+'''
+ROUGHNESS GRID (LAND USE + STEEP SLOPES)
+'''
+# Slope
+arcpy.Slope_3d(dem_buildings, slope_grid, "DEGREE", 1.0)
+arcpy.AddMessage("Slopes have been calculated.")
+
+# Reclassify
+additional_roughness = Reclassify(slope_grid, "Value", RemapRange([[0, inclination, "NODATA"],
+                                                                   [inclination, 180, 998]]))
+additional_roughness.save(steep_slopes_grid)
+arcpy.AddMessage("Raster has been reclassified.")
+
+# Clip
+arcpy.Clip_management(steep_slopes_grid, extent, steep_slopes_grid_clip, catchment, "#", "ClippingGeometry",
+                      "NO_MAINTAIN_EXTENT")
+arcpy.AddMessage("Land use raster has been clipped.")
+
+# Mosaic to new raster
+arcpy.MosaicToNewRaster_management("steep_slopes_grid_clip; landuse_grid", workspace, "roughness_grid", "",
+                                   "16_BIT_UNSIGNED", cell_size, 1, "FIRST", "FIRST")
+arcpy.AddMessage("Roughness raster has been built.")
+
+'''
+DATA EXPORT
+'''
+# Model domain to ASCII
+output_file = output_folder + str("/model_domain.asc")
+arcpy.RasterToASCII_conversion("model_domain_grid", output_file)
+arcpy.AddMessage("Model domain raster has been exported to ASCII.")
+
+# Land use to ASCII
+output_file = output_folder + str("/land_use.asc")
+arcpy.RasterToASCII_conversion("landuse_grid", output_file)
+arcpy.AddMessage("Land use raster has been exported to ASCII.")
+
+# Land use to ASCII
+output_file = output_folder + str("/roughness.asc")
+arcpy.RasterToASCII_conversion("roughness_grid", output_file)
+arcpy.AddMessage("Roughness raster has been exported to ASCII.")
 
 # Delete temp files
 arcpy.Delete_management(rasterized_buildings)
 arcpy.Delete_management(rasterized_buildings_calc)
-arcpy.Delete_management(rasterized_buildings_clip)
-arcpy.Delete_management(slope_grid)
-arcpy.Delete_management(slope_roughness)
+arcpy.Delete_management(dem_buildings)
 arcpy.Delete_management(clipped_dem)
 arcpy.Delete_management(catchment_buffer)
+arcpy.Delete_management(rasterized_buffer)
+arcpy.Delete_management(land_use_clip)
+arcpy.Delete_management(slope_grid)
+arcpy.Delete_management(steep_slopes_grid)
+arcpy.Delete_management(steep_slopes_grid_clip)
