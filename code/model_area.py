6d84c4edcf7d9777d17f666c18014a0dfd8b139a
Piotr <38719982+Daldek@users.noreply.github.com>
2021-10-20 22:32:09 +0200

    Add files via upload
    Hotfix

--- code/model_area.py
+++ code/model_area.py
@@ -6,11 +6,8 @@ input_raster = arcpy.GetParameterAsText(1)  # "filled_channels" raster from "dem
 rise = int(arcpy.GetParameterAsText(2))  # Building heights
 catchments = arcpy.GetParameterAsText(3)  # Selected catchments. From "catchment_processing.py"
 buildings = arcpy.GetParameterAsText(4)  # Polygon feature class
-landuse_raster = arcpy.GetParameterAsText(5)  # Raster
-inclination = arcpy.GetParameterAsText(6)
-# Unit: degree. For slopes greater then specified inclination, new land use class will be created
-buffer_distance = int(arcpy.GetParameterAsText(7))  # Meters
-output_folder = arcpy.GetParameterAsText(8)  # Where the ASCII files will be saved
+buffer_distance = int(arcpy.GetParameterAsText(5))  # Meters
+output_folder = arcpy.GetParameterAsText(6)  # Where the ASCII files will be saved
 
 # Env settings
 arcpy.env.workspace = workspace
@@ -21,5 +18,5 @@ arcpy.env.cellSize = input_raster
 arcpy.env.nodata = "NONE"
 
 domain_creation(workspace, input_raster, rise, catchments,
-                buildings, landuse_raster, inclination, buffer_distance, output_folder)
+                buildings, buffer_distance, output_folder)
 arcpy.AddMessage('Success!')
