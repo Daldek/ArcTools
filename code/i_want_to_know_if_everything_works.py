0dc7e35ded0a7dbd122f5766f9fc21c95017e250
Daldek <piotr.de.bever@gmail.com>
2023-05-10 16:18:04 +0200

    Workspace automation

--- code/i_want_to_know_if_everything_works.py
+++ code/i_want_to_know_if_everything_works.py
@@ -55,65 +55,73 @@ arcpy.env.snapRaster = in_dem
 arcpy.env.cellSize = in_dem
 arcpy.env.nodata = "NONE"
 
-# 1 - raster manipulation (without AgreeDEM)
+# 1 - dedicated workspace
+w = AutomationWorkspace(workspace)
+w.verification()
+
+# 2 - raster manipulation (without AgreeDEM)
 raster_manipulation(workspace, in_dem, in_culverts, maximum_distance, smooth_drop, sharp_drop, in_lake, False)
 arcpy.AddMessage("Step 1 - complete!\n")
 
-# 2 - raster manipulation (with AgreeDEM)
+# 3 - raster manipulation (with AgreeDEM)
 raster_manipulation(workspace, in_dem, in_culverts, maximum_distance, smooth_drop, sharp_drop, in_lake, True)
 agree_dem = workspace + r"/script_agreeDEM"
 filled_channels = workspace + r"/script_filled_channels"
 arcpy.AddMessage("Step 2 - complete!\n")
 
-# 3 - catchment processing (without additional rasters)
+# 4 - catchment processing (without additional rasters)
 catchment_delineation(workspace, in_dem, agree_dem, catchment_area)
 catchments = workspace + r"/catchments"  # I do not want to use all catchments anymore
 arcpy.AddMessage("Step 3 - complete!\n")
 
-# 4 - Generating longest flowpath for a catchment
+# 5 - Generating longest flowpath for a catchment
 flow_dir_raster = workspace + r"/script_flow_dir"
 longest_flow_path(workspace, flow_dir_raster, '')
 arcpy.AddMessage("Step 4 - complete!\n")
 
-# 5 - Generating longest flowpath for a catchment (additional input data)
+# 6 - Generating longest flowpath for a catchment (additional input data)
 flow_dir_raster = workspace + r"/script_flow_dir"
 flow_ln_raster = workspace + r"/script_flow_ln"
 longest_flow_path(workspace, flow_dir_raster, flow_ln_raster)
 arcpy.AddMessage("Step 5 - complete!\n")
 
-# 6 - domain creation (with catchment simplification)
+# 7 - domain creation (with catchment simplification)
 domain_creation(workspace, in_dem, rise, in_selected_catchment,
                 in_buildings, buffer_distance, output_folder, True)
 arcpy.AddMessage("Step 6 - complete!\n")
 
-# 7 - domain creation (without catchment simplification)
+# 8 - domain creation (without catchment simplification)
 domain_creation(workspace, in_dem, rise, in_selected_catchment,
                 in_buildings, buffer_distance, output_folder, False)
 arcpy.AddMessage("Step 7 - complete!\n")
 
 buffer_distance = 0
-# 8 - domain creation (with catchment simplification, but without buffer)
+# 9 - domain creation (with catchment simplification, but without buffer)
 domain_creation(workspace, in_dem, rise, in_selected_catchment,
                 in_buildings, buffer_distance, output_folder, True)
 arcpy.AddMessage("Step 8 - complete!\n")
 
-# 9 - export to ascii and validation
+# 10 - export to ascii and validation
 mask = r"../temp/test_output/bathymetry.asc"
 in_rasters = [r'../data.gdb/in_landuse_sweref1800', r'../data.gdb/in_raster_sweref1800']
 mask_and_export(mask, in_rasters, output_folder)
 arcpy.AddMessage("Step 9 - complete!\n")
 
-# 10 - Fastighetskartan. Simplify and export
+# 11 - Fastighetskartan. Simplify and export
 fastighetskartan_markytor_simplifed(workspace, in_fastighetskartan_markytor)
 arcpy.AddMessage("Step 10 - complete!\n")
 
-# 11 - infrastructure at risk
+# 12 - infrastructure at risk
 infrastructure_at_risk(workspace, [10, 100.0], in_buildings2, in_depth_sweref99tm)
 arcpy.AddMessage("Step 11 - complete!\n")
 
 # clean up
 arcpy.AddMessage('One moment, please...')
 arcpy.Delete_management("in_memory")
+try:
+    arcpy.Delete_management(w.path)
+except:
+    pass
 try:
     os.remove('log')
 except:
